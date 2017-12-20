fef400e8f2d54b1beaee0ed96d4e374aef96ac95
fr1sk <fr1sk@live.com>
2017-12-20 01:22:14 +0100

    date-log added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const mailer = require('nodemailer');
-const date = new Date();
+let date = require('./date')
 
 function checker(){
-    
+    let time = new Date();
+    console.log(date.formatDate(time, "dddd h:mmtt d MMM yyyy")+" => ");
     setTimeout(checker, 1000);
 }
 
